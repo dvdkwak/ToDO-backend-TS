cd2bdb3417aa241dcef5025faeac86ea4c10fcc1
dvdkwak <david.kwakernaak@student.hu.nl>
2022-11-29 19:49:38 +0100

    [todo:read] Cleaned the code, added commends and now retrieving all todo's is possible.

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,22 @@ import * as dotenv from 'dotenv';
 import router from './apiRouter';
 import bodyParser from 'body-parser';
 
+// Retrieving .env variables.
 dotenv.config();
 
+// Instantiating Prisma (ORM) and express app objects.
 const prisma = new PrismaClient();
 const app = express();
 
+// Applying the body parser so data in multiple formats is accepted by the api.
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
 
+// Applying the api zouter file.
 app.use(router);
 
+// Booting the api on the set port from .env file.
 app.listen(process.env.PORT, () => {
   // tslint:disable-next-line:no-console
   console.log(`Server running on localhost:${process.env.PORT}`);
